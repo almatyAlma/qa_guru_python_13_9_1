34210a64937402ccd8c8d1a15c7743ed65ab3640
Bekbergenova Alma <alma02071988@gmail.com>
2024-06-17 20:41:01 +0500

    high-level-step-objects

--- tests/test_demoqa_form.py
+++
@@ -1,8 +0,0 @@
-from demoqa_tests.data import users
-from demoqa_tests.pages.registration_page import RegistrationPage
-
-
-def test_demoqa_form():
-    registration_page = RegistrationPage()
-    admin = users.admin
-
