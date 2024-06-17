f0631fb674d7537a7609c3a658dada8622074d75
Bekbergenova Alma <alma02071988@gmail.com>
2024-06-17 19:35:26 +0500

    high-level-step-objects

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,11 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.config.window_height = 720
-    browser.config.window_width = 1280
+@pytest.fixture(scope="function", autouse=True)
+def browser_params():
+    browser.config.base_url = "https://demoqa.com"
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
 
     yield
-
     browser.quit()
