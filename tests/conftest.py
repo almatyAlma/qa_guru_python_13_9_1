d2e605f39326acd47183e82ba2776d0bac0a9513
Bekbergenova Alma <alma02071988@gmail.com>
2024-06-24 14:14:40 +0500

    fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 
 from selenium import webdriver
@@ -28,7 +29,27 @@ def setup_browser():
     browser.config.base_url = "https://demoqa.com"
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    yield
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser.config.driver = driver
+    yield browser
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)
