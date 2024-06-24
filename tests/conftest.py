f6bc13e53b42c1a11a0f768955ccde2327519798
Bekbergenova Alma <alma02071988@gmail.com>
2024-06-24 12:27:19 +0500

    pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,19 @@
 import pytest
-
+from selene import browser
+from utils import attach
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import browser
 
-from utils import attach
 
-@pytest.fixture(scope='function',autouse=True)
-def browser_params():
-    browser.config.base_url = "https://demoqa.com"
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('--headless')
+    browser.config.driver_options = driver_options
+    browser.config.timeout = 15
     browser.config.window_height = 1080
     browser.config.window_width = 1920
-    yield
-    browser.quit()
-def setup_browser(request):
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -25,7 +25,7 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
